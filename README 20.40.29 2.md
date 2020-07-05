5d738d7b23480485328f5c9a549a334c1785b435
DoHerasYang <doherasyanng@gmail.com>
2020-07-06 00:47:37 +0100

    Update Login and Logup Page

---
+++ README 20.40.29 2.md
@@ -0,0 +1,94 @@
+
+
+---
+
+## Master Project - Design a Safe Driving Program by React Native
+
+---
+
+**Dependency:**
+
+1. Firstly, You should use the `npm install -g @aws-amplify/cli` command to add the `aws-amplify` into this project file.
+2. Use the command `amplify configure` and follow the Access AWS page hints.
+3. 
+
+---
+
+**Command Hint**:
+
+1.`sudo npm i --save react-bootstrap`
+
+2.`sudo npm install -g @aws-amplify/cli`
+
+3.`yarn add @aws-amplify/api @aws-amplify/pubsub`
+
+4.` yarn add aws-amplify @aws-amplify/core aws-amplify-react-native amazon-cognito-identity-js @react-native-community/netinfo`
+
+5.Unistall the wrong version: `npm uninstall @react-native-community/netinfo` 
+
+6.Install the Correct Version: `npm install @react-native-community/netinfo@5.9.2`
+
+---
+
+### 1. Tasks 
+
+<TODO>
+
+<date>  25th,Apirl,2020  </date>
+
+<content> Tutorial about the React Native (Expo) / My First Reactive Native Project</content>
+
+<date>   18th,June,2020  </date>
+<content> Finish the Log in page and Sign Up page </content>
+
+
+</TODO>
+
+
+
+### 2. Project Progress
+
+#### 23th,Apirl,2020
+
++ Finishing establishing the first React Native project via the Git application and Expo command line.
++ Install the dependencies( AWS ) and GraphQL
++ Run my first demo application on iOS simulator and Android Device
+
+#### 25th,Apirl,2020
+
++ Finish initializing the AWS configuration
++ Finish configuration the GraphQL. And learn the tutorial of GraphQL Schema.
+
+#### 26th,Apirl,2020
+
++ Fix the problem that the Git can't recognize the React Native project files and dependencies.
++ Rebuild the project structure
+
+#### 15th,Apirl,2020
++ Finish the Basic Page registration UI and function and test.
+
+#### 18th,June,2020
++ Finish the Basic UI about the Signin/SignUp page. And Generally finished 
++ Understand the Basic rule of the Expo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ container/Login.js
@@ -0,0 +1,121 @@
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { Auth } from 'aws-amplify'
+import Amplify from '@aws-amplify/core'
+import config from '../aws-exports'
+import { Button, FormGroup, FormControl } from "react-bootstrap";
+
+Amplify.configure(config)
+export default class Login extends React.Component {
+
+    state = {
+        username: '',
+        password: '',
+        user:{},
+    };
+
+    // validateForm(input_string){
+    //     const reg =  /^[a-z][0-9][A-Za-z0-9]{10}$/;
+    //     let tag = input_string.length>0 && reg.test(input_string);
+    //     if (tag){
+    //
+    //     }else{
+    //         alert("Wrong Input...")
+    //     }
+    // }
+
+    async handleSubmit(event){
+        const {username,password} = this.state;
+        try {
+            const user = await Auth.signIn(username,password);
+            this.setState({user})
+            alert("Successfully Log in!");
+        }catch (e) {
+            alert(e.message);
+        }
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.logo}>Sticker</Text>
+                <View style={styles.inputView} >
+                    <TextInput
+                        value={this.state.username}
+                        style={styles.inputText}
+                        placeholder="Username"
+                        placeholderTextColor="#003f5c"
+                        keyboardType='email-address'
+                        onChangeText={(username) => this.setState({username})}/>
+                </View>
+                <View style={styles.inputView} >
+                    <TextInput
+                        value={this.state.password}
+                        secureTextEntry={true}
+                        style={styles.inputText}
+                        placeholder="Password"
+                        keyboardType="default"
+                        placeholderTextColor="#003f5c"
+                        onChangeText={(password) => this.setState({password})}/>
+                </View>
+                <TouchableOpacity>
+                    <Text style={styles.forgot}>Forgot Password?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.loginBtn}
+                    onPress={this.handleSubmit.bind(this)}
+                    activeOpacity = {0.3}>
+                    <Text style={styles.loginText}>LOG IN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.loginText}>Sign up</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#290066',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo:{
+        fontWeight:"bold",
+        fontSize:50,
+        color:"#fb5b5a",
+        marginBottom:40
+    },
+    inputView:{
+        width:"80%",
+        backgroundColor:"#d9d9d9",
+        borderRadius:25,
+        height:50,
+        marginBottom:20,
+        justifyContent:"center",
+        padding:20
+    },
+    inputText:{
+        height:50,
+        color:"black"
+    },
+    forgot:{
+        color:"white",
+        fontSize:11
+    },
+    loginBtn:{
+        width:"80%",
+        backgroundColor:"#fb5b5a",
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:30,
+        marginBottom:10
+    },
+    loginText:{
+        color:"white"
+    }
+});
